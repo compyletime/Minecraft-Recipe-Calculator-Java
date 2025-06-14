9366bb7f66331ad4e20328aee3865c5d347f3f75
Felix Schrage <compyle-dev@proton.me>
2025-06-14 20:07:53 +0200

    Initial code commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
